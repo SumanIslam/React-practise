60e16463d539666beb327aed311dbddabaaee74c
suman Islam <sumunahmed1416@gmail.com>
2020-11-03 16:07:01 +0600

    working with react standard layout

--- colt stele course practise/index.js
+++ colt stele course practise/index.js
@@ -57,20 +57,33 @@
 
 // .. conditional in jsx
 
-function getNum() {
-  return Math.floor(Math.random() * 10) + 1;
-}
-class NumPicker extends React.Component {
-  render() {
-    const num = getNum();
-    return(
+// function getNum() {
+//   return Math.floor(Math.random() * 10) + 1;
+// }
+// class NumPicker extends React.Component {
+//   render() {
+//     const num = getNum();
+//     return(
+//       <div>
+//         <h1>Your Number is: {num}</h1>
+//         <p>Here, Lucky number is 7. Let's see you are lucky or not!</p>
+//         <p>{num === 7 ? 'Congrats! You are lucky': 'Oops! You are unlucky'}</p>
+//       </div>
+//     );
+//   }
+// }
+
+//standard react layout
+
+class App extends React.Component {
+  render(){
+    return (
       <div>
-        <h1>Your Number is: {num}</h1>
-        <p>Here, Lucky number is 7. Let's see you are lucky or not!</p>
-        <p>{num === 7 ? 'Congrats! You are lucky': 'Oops! You are unlucky'}</p>
+        <Hello/>
+        <NumPicker />
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<NumPicker/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
